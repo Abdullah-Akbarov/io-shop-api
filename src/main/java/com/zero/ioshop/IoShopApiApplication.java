134e0b5311676f170f230be8e617d12d99de7b96
Abdulloh Akbarov <109663144+Abdullah-Akbarov@users.noreply.github.com>
2022-11-18 16:53:08 +0500

    first commit

---
+++ src/main/java/com/zero/ioshop/IoShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.zero.ioshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoShopApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zero/ioshop/IoShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.ioshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
