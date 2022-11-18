0d7eb7aec276eb695546cd0981379c8a67c71ec7
Abdulloh Akbarov <109663144+Abdullah-Akbarov@users.noreply.github.com>
2022-11-18 18:02:09 +0500

    entities created for user service close #2

---
+++ user-service/src/main/java/com/zero/ioshop/userservice/entity/Role.java
@@ -0,0 +1,35 @@
+package com.zero.ioshop.userservice.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
---
+++ user-service/src/main/java/com/zero/ioshop/userservice/entity/User.java
@@ -0,0 +1,50 @@
+package com.zero.ioshop.userservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity(name = "users")
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+    private String photo;
+    @Column(columnDefinition = "boolean default true")
+    private boolean isActive;
+    @ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String username, String password, String firstName, String lastName, String email) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+}
